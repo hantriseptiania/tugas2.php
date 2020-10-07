9fce5c1dba6a73e0c1f4cc7c5c0e792ce142baa4
hantriseptiania <71365751+hantriseptiania@users.noreply.github.com>
2020-10-07 10:14:43 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,47 @@
+<?php
+	$makanan1 = "Cireng";
+	$makanan2 = "Es Duren";
+	$makanan3 = "Es Milo";
+	$makanan4 = "Es Teh";
+	$makanan5 = "Piscok";
+
+	$harga1 = 1000;
+	$harga2 = 15000;
+	$harga3 = 20000;
+	$harga4 = 5000;
+	$harga5 = 1000;
+
+	echo"<h1>Daftar Makanan</h1>";
+	echo"<table border = '1'>
+		<tr>
+			<td>No</td>
+			<td>Nama Menu</td>
+			<td>Harga</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>".$makanan1."</td>
+			<td>".$harga1."</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>".$makanan2."</td>
+			<td>".$harga2."</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>".$makanan3."</td>
+			<td>".$harga3."</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>".$makanan4."</td>
+			<td>".$harga4."</td>
+		</tr>
+		<tr>
+			<td>5</td>
+			<td>".$makanan5."</td>
+			<td>".$harga5."</td>
+		</tr>
+	</table>";
+?>
